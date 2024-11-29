c85f1ba2e662920278f3da4dd2bbe6860343bfc2
Savvsavv <demesasavannah@gmail.com>
2024-11-29 20:39:51 +0800

    Create Activity8.py

---
+++ Activity8.py
@@ -0,0 +1,17 @@
+password= input("Enter you secret pssword: ")
+
+if password.lower()=="ahele":
+  print("Access Granted")
+  print("Enjoy using the System ^_^")
+
+elif password.lower() =="coding":
+  print("Access Granted")
+  print("Welcome Back, Master")
+
+elif passwod.lower()=="graduate":
+  print("Access Granted")
+  print("Hello Once Again")
+
+else:
+  print("Access Denied")
+print("System Exit")
