13cb03f0394624a4ed7efc320a8de0c5290f90a4
Savvsavv <demesasavannah@gmail.com>
2024-11-29 20:20:19 +0800

    Create Activity4.py

---
+++ Activity4.py
@@ -0,0 +1,13 @@
+num1 = eval(input("Enter a number--> "))
+
+num2 =eval(input("Enter another number --> "))
+
+add= num1+num2
+minus = num1 - num2
+times= num1 *num2 
+divide= num1 /num2 
+
+print("The sum of", num1, "and ", num2, "is " ,add )
+print("The difference of", num1, "and ", num2, "is " ,minus )
+print("The product of", num1, "and ", num2, "is " ,times )
+print("The dqquotient of", num1, "and ", num2, "is " ,divide )
