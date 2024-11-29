dd97b4a5d9777bffa83d7a6f17b319c537073ebf
Savvsavv <demesasavannah@gmail.com>
2024-11-29 21:20:54 +0800

    Create Activity10.py

---
+++ Activity10.py
@@ -0,0 +1,25 @@
+DLL =input("Are you a current student of DLL [yes/no] ")
+
+if DLL.lower()=="yes":
+  print("Welcome to Dalubhasaan ng Lungsod ng Lucena")
+  isBSIT=input("Are you currently enrolled in Bachelor of Science in Infomation Technology [yes/no]")
+  
+  if isBSIT.lower()=="yes":
+    print("What a Beautiful Day! Glad to have you in the department of BSIT")
+    print("Please fill up the second form")
+
+    isLevel= input("What current year level are right now in DLL? \n F-FRESHMEN \n S-SOPHOMORE \n J-JUNIOR \n SS-SENIOR \n Please input your answer here --> ")
+    if isLevel.lower() =="f":
+      print("WELCOME ABOARD FRESHMEN!! HOPE YOU ENJOY YOUR STAY")
+    elif isLevel.lower() =="s":
+      print("WELCOME SOPHOMORE! ")
+    elif isLevel.lower() =="j":
+      print("WELCOME JUNIOR")
+    elif isLevel.lower() =="ss":
+      print("WELCOME SENIOR")
+  else:
+    print("Sorry you are not welcome here")
+
+else:
+  print("Thanks for stopping by ^_^")
+  
