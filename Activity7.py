b02d4ca377f8fe8c109619d93a6fc7ee4e0ea1f1
Savvsavv <demesasavannah@gmail.com>
2024-11-29 20:31:37 +0800

    Create Activity7.py

---
+++ Activity7.py
@@ -0,0 +1,16 @@
+#conditional statements
+gold=0
+miner=input("Hola! Please enter your name here --> ")
+
+#answerable by yes or no
+
+isminer= input("Did you mine gold today???)
+               
+#introduction to decision structure and relational operators
+
+if isminer.lower() =="yes":
+    gold+=1
+    print(f"Hi {miner}, today you have a total of {gold} gold")
+else:
+    print(f"Hi {miner}, today you have a total of {gold} gold")
+               
