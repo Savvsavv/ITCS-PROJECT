5b0afbc3754e1c4889a7e18b9c1ed111d0d4d90f
Savvsavv <demesasavannah@gmail.com>
2024-12-01 19:53:22 +0800

    Create Activity15.py

---
+++ Activity15.py
@@ -0,0 +1,9 @@
+for x in range (0,11):
+  print(x,"\t", end=" ")
+  for y in range (0,x):
+    print ("*",end=" ")
+  print ()
+for x in range (10,0,-1):
+  for y in range (0,x):
+    print(" * ",end=" ")
+  print()
