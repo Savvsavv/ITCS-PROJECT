208c1b96371904099612d01f4e7604c0070cf59f
Savvsavv <demesasavannah@gmail.com>
2024-12-01 19:48:28 +0800

    Update Activity14.py

--- Activity14.py
+++ Activity14.py
@@ -1,2 +1,12 @@
-for x in range:
+for x in range(1,15):
+  print(x, end= "")
+  for y in range (1,15):
+    print("*",end="")
+  print("\n")
+
+for x in range (0,10):
+  print("@",end="")
+  for y in range (0,10):
+    print(x,end="")
+  print("")
   
