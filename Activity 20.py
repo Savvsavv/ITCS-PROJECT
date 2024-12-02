f9413b06df763296dee767edfef838dfa3d4078b
Savvsavv <demesasavannah@gmail.com>
2024-12-02 20:28:05 +0800

    Create Activity 20.py

---
+++ Activity 20.py
@@ -0,0 +1,7 @@
+isContinue=True
+nt=0
+import os
+while isContinue ==True:
+  ask= input("WOULD YOU LIKE TO ADD ANOTHER TRIANGLE [y/n] -->")
+  if ask.lower()=="no":
+    print("Program Terminated ")
