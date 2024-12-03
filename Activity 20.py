321bd1ebcdf1da6a4c215b058aa6498c3d5277cc
Savvsavv <demesasavannah@gmail.com>
2024-12-03 21:16:42 +0800

    Update Activity 20.py

--- Activity 20.py
+++ Activity 20.py
@@ -5,3 +5,9 @@ while isContinue ==True:
   ask= input("WOULD YOU LIKE TO ADD ANOTHER TRIANGLE [y/n] -->")
   if ask.lower()=="no":
     print("Program Terminated ")
+    break
+    isContinue =False
+  elif ask.lower()=="yes":
+    os.system('cls')
+    nt+=1
+    for x in range (1,5):
