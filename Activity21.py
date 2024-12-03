b57372edc1028f83fc6ecddc84321a0afb68192b
Savvsavv <demesasavannah@gmail.com>
2024-12-03 21:15:07 +0800

    Update Activity21.py

--- Activity21.py
+++ Activity21.py
@@ -1,4 +1,12 @@
 go=True
 count=0
 while go ==True:
-name =input("Please enter a name:")
+   name =input("Please enter a name:")
+   if name.lower() =="stop":
+      print("Loop has now stopped")
+      print(f"you have entered {count} number of name")
+      break
+      go==False
+   else:
+      count+=1
+      continue
