3932ba9880fb40c176d698b1107d44885515f15e
Savvsavv <demesasavannah@gmail.com>
2024-12-05 22:09:56 +0800

    Update Activity 20.py

--- Activity 20.py
+++ Activity 20.py
@@ -11,3 +11,15 @@ while isContinue ==True:
     os.system('cls')
     nt+=1
     for x in range (1,5):
+      for y in range (1,nt +1):
+        for z in range (1,x+1(:
+          print("*",end=" )
+        for a in range (5,x,-1):
+          print (" ", end=" ")
+        print (end= " ")
+      print()
+    continue
+
+  else:  
+    print ("TRY AGAIN")
+    continue
