b398937c0965012431055e0d4219ef9ca50fbb56
Savvsavv <demesasavannah@gmail.com>
2024-12-15 23:31:43 +0800

    Update Activity22.py

--- Activity22.py
+++ Activity22.py
@@ -1,1 +1,14 @@
+import os
+
 def panghello():
+  print("\nHello IT1A")
+
+iscontinue =True
+
+while isContinue:
+  print("\nSelect from the following code \n1-PANGHELLO")
+
+ask= input("Which would you like to run?elect from the options above")
+
+if ask 1 =="1":
+  panghello()
