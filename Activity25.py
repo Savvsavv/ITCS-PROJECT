c55b34cd48d056be75226558a1f11b5c2fb28df5
Savvsavv <demesasavannah@gmail.com>
2024-12-15 23:28:35 +0800

    Update Activity25.py

--- Activity25.py
+++ Activity25.py
@@ -1,2 +1,17 @@
-#vocab
-print("")
+#LIST
+
+fruit1="apples"
+fruit2="orange"
+fruit3="starapple"
+fruit4="duha"
+
+fruits= ["apples","orange","starapple","duha"]
+print("fruits")
+
+#access individuality item
+print([f"my favorite fruit growing up is {fruits[3]}")
+#add more item to the list
+fruits.append("longgan")
+fruits.insert(3,"banana")
+print(fruits)
+
