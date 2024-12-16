8defd351ad44d693eb6740c5ce2253889d2528f6
Savvsavv <demesasavannah@gmail.com>
2024-12-17 07:53:44 +0800

    Update code_challenge5.py

--- code_challenge5.py
+++ code_challenge5.py
@@ -1,11 +1,13 @@
-print ("FAHRENHEIT TO CELSIUS CONVERTER PROGRAM")
-print("-------------------------------------------")
-fahrenheit = eval(input("Enter temperature in farenheit --> "))
+def Code_Challenge5():
+    print ("FAHRENHEIT TO CELSIUS CONVERTER PROGRAM")
+    print("-------------------------------------------")
+    fahrenheit = eval(input("Enter temperature in farenheit --> "))
 
-celsius = ((fahrenheit -32) *5)/9
- 
-print("\n" ,fahrenheit, "degrees Fahrenheit converted to celsius is" , celsius, "degrees")
+    celsius = ((fahrenheit -32) *5)/9
+    
+    print("\n" ,fahrenheit, "degrees Fahrenheit converted to celsius is" , celsius, "degrees")
 
-print (f"\n {fahrenheit} degress Farenheit converted to celsius is {round(celsius, 2)} degrees")
+    print (f"\n {fahrenheit} degress Farenheit converted to celsius is {round(celsius, 2)} degrees")
 
-print("---------------------------------------------")
+    print("---------------------------------------------")
+Code_Challenge5()
