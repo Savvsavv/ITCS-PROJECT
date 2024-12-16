a71e489b85dd420c0bcc2aab7ee9727982517349
Savvsavv <demesasavannah@gmail.com>
2024-12-17 07:19:25 +0800

    Update Activity19.py

--- Activity19.py
+++ Activity19.py
@@ -1,13 +1,16 @@
-import os
-tuloy=True
-os.system('cls')
+def Activity19():
+  import os
+  tuloy=True
+  os.system('cls')
 
-while tuloy ==True:
-  name =input("Please enter a name-->")
+  while tuloy ==True:
+    name =input("Please enter a name-->")
 
-  if name.lower() =="stop":
-     print("Program Terminated ")
-     break 
-     tuloy=False
- else:
-     continue
+    if name.lower() =="stop":
+      print("Program Terminated ")
+      break 
+      tuloy=False
+    else:
+      continue
+      
+Activity19()
