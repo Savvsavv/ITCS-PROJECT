c5c34986b01845924dc167e4a90b617f5990fa13
Savvsavv <demesasavannah@gmail.com>
2024-12-16 22:21:07 +0800

    Update Activity20.py

--- Activity20.py
+++ Activity20.py
@@ -1,25 +1,27 @@
-isContinue=True
-nt=0
-import os
-while isContinue ==True:
-  ask= input("WOULD YOU LIKE TO ADD ANOTHER TRIANGLE [y/n] -->")
-  if ask.lower()=="no":
-    print("Program Terminated ")
-    break
-    isContinue =False
-  elif ask.lower()=="yes":
-    os.system('cls')
-    nt+=1
-    for x in range (1,5):
-      for y in range (1,nt +1):
-        for z in range (1,x+1):
-          print("*",end=" )
-        for a in range (5,x,-1):
-          print (" ", end=" ")
-        print (end= " ")
-      print()
-    continue
+def Activity20():
+  isContinue=True
+  nt=0
+  import os
+  while isContinue ==True:
+    ask= input("WOULD YOU LIKE TO ADD ANOTHER TRIANGLE [y/n] -->")
+    if ask.lower()=="no":
+      print("Program Terminated ")
+      break
+      isContinue =False
+    elif ask.lower()=="yes":
+      os.system('cls')
+      nt+=1
+      for x in range (1,5):
+        for y in range (1,nt +1):
+          for z in range (1,x+1):
+            print("*",end=" ")
+          for a in range (5,x,-1):
+            print (" ", end=" ")
+          print (end= " ")
+        print()
+      continue
 
-  else:  
-    print ("TRY AGAIN")
-    continue
+    else:  
+      print ("TRY AGAIN")
+      continue
+Activity20()
