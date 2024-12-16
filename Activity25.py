387377eab33291d0d9eb116b72410acc82d51923
Savvsavv <demesasavannah@gmail.com>
2024-12-17 07:43:12 +0800

    Update Activity25.py

--- Activity25.py
+++ Activity25.py
@@ -1,17 +1,19 @@
-#LIST
+def Activity25():
+    #LIST
 
-fruit1="apples"
-fruit2="orange"
-fruit3="starapple"
-fruit4="duha"
+    fruit1="apples"
+    fruit2="orange"
+    fruit3="starapple"
+    fruit4="duha"
 
-fruits= ["apples","orange","starapple","duha"]
-print("fruits")
+    fruits= ["apples","orange","starapple","duha"]
+    print("fruits")
 
-#access individuality item
-print([f"my favorite fruit growing up is {fruits[3]}")
-#add more item to the list
-fruits.append("longgan")
-fruits.insert(3,"banana")
-print(fruits)
+    #access individuality item
+    print(f"my favorite fruit growing up is {fruits[3]}")
+    #add more item to the list
+    fruits.append("longgan")
+    fruits.insert(3,"banana")
+    print(fruits)
 
+Activity25()
