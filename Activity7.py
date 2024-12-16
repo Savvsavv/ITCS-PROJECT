6d2d228d22057936600185032e61dc1e2972e358
Savvsavv <demesasavannah@gmail.com>
2024-12-16 21:51:57 +0800

    Update Activity7.py

--- Activity7.py
+++ Activity7.py
@@ -1,16 +1,16 @@
-#conditional statements
-gold=0
-miner=input("Hola! Please enter your name here --> ")
+def Activity7(): #ACTIVITY 7 CODE
+    #conditional statements
+    gold=0
+    miner=input("Hola! Please enter your name here --> ")
 
-#answerable by yes or no
+    #answerable by yes or no
 
-isminer= input("Did you mine gold today???)
-               
-#introduction to decision structure and relational operators
+    isminer= input("Did you mine gold today???")
+                
+    #introduction to decision structure and relational operators
 
-if isminer.lower() =="yes":
-    gold+=1
-    print(f"Hi {miner}, today you have a total of {gold} gold")
-else:
-    print(f"Hi {miner}, today you have a total of {gold} gold")
-               
+    if isminer.lower() =="yes":
+        gold+=1
+        print(f"Hi {miner}, today you have a total of {gold} gold")
+    else:
+        print(f"Hi {miner}, today you have a total of {gold} gold")
