aa440a11a091deb278fe129f521cbf41f192685a
Savvsavv <demesasavannah@gmail.com>
2024-12-16 21:53:23 +0800

    Update Activity8.py

--- Activity8.py
+++ Activity8.py
@@ -1,17 +1,19 @@
-password= input("Enter you secret pssword: ")
+def Activity8(): #ACTIVITY 8 CODE
+  password= input("Enter you secret pssword: ")
 
-if password.lower()=="ahele":
-  print("Access Granted")
-  print("Enjoy using the System ^_^")
+  if password.lower()=="ahele":
+    print("Access Granted")
+    print("Enjoy using the System ^_^")
 
-elif password.lower() =="coding":
-  print("Access Granted")
-  print("Welcome Back, Master")
+  elif password.lower() =="coding":
+    print("Access Granted")
+    print("Welcome Back, Master")
 
-elif passwod.lower()=="graduate":
-  print("Access Granted")
-  print("Hello Once Again")
+  elif passwod.lower()=="graduate":
+    print("Access Granted")
+    print("Hello Once Again")
 
-else:
-  print("Access Denied")
-print("System Exit")
+  else:
+    print("Access Denied")
+  print("System Exit")
+Activity8()
