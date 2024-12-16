dff2b427afba3c262064d1b85ed1301b09609c70
Savvsavv <demesasavannah@gmail.com>
2024-12-16 22:12:03 +0800

    Update Activity17.py

--- Activity17.py
+++ Activity17.py
@@ -1,7 +1,8 @@
 #create a multiplication table
-
-column =eval(input("Enter number of columns -->" ))
-for x in range (1,11):
-  for y in range(1, column+1):
-    print(f"{x} x {y} = {x*y}", end ="\t")
-  print()
+def Activity17():
+  column =eval(input("Enter number of columns -->" ))
+  for x in range (1,11):
+    for y in range(1, column+1):
+      print(f"{x} x {y} = {x*y}", end ="\t")
+    print()
+Activity17()
