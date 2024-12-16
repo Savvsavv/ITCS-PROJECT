65bcac94dcf07a1a9b114d4e087a5b49359c244d
Savvsavv <demesasavannah@gmail.com>
2024-12-17 07:37:48 +0800

    Update Activity23.py

--- Activity23.py
+++ Activity23.py
@@ -1,10 +1,14 @@
 #demo of doc string
-def factorial (number):
-  """This function's purpose is to compute /calculate the factorial of any number given """
-    fact =1
-    for x in range (number, 0,-1):
-       fact *=x
-#print(f"the factorial of 13 is (factorial(13)")
+def Activity23():
+  def factorial(number):
+      """This function's purpose is to compute/calculate the factorial of any number given."""
+      fact = 1
+      for x in range(number, 0, -1):
+        fact *= x
+      return fact
+    
+  print(f"the factorial of 13 is (factorial(13)")
 
-help (factorial)
-help(print)
+  # help (factorial)
+  # help(print)
+Activity23()
