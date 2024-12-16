6c22021ed5c7946a2c7be349b11d0f3c3776124c
Savvsavv <demesasavannah@gmail.com>
2024-12-16 21:40:47 +0800

    Update Activity4.py

--- Activity4.py
+++ Activity4.py
@@ -1,13 +1,16 @@
-num1 = eval(input("Enter a number--> "))
+def Activity4():
+	num1 = eval(input("Enter a number--> "))
 
-num2 =eval(input("Enter another number --> "))
+	num2 =eval(input("Enter another number --> "))
 
-add= num1+num2
-minus = num1 - num2
-times= num1 *num2 
-divide= num1 /num2 
+	add= num1+num2
+	minus = num1 - num2
+	times= num1 *num2 
+	divide= num1 /num2 
 
-print("The sum of", num1, "and ", num2, "is " ,add )
-print("The difference of", num1, "and ", num2, "is " ,minus )
-print("The product of", num1, "and ", num2, "is " ,times )
-print("The dqquotient of", num1, "and ", num2, "is " ,divide )
+	print("The sum of", num1, "and ", num2, "is " ,add )
+	print("The difference of", num1, "and ", num2, "is " ,minus )
+	print("The product of", num1, "and ", num2, "is " ,times )
+	print("The dqquotient of", num1, "and ", num2, "is " ,divide )
+
+Activity4()
