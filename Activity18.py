b88987efb080aa30b86f2bcb185b43263e503600
Savvsavv <demesasavannah@gmail.com>
2024-12-16 22:14:36 +0800

    Update Activity18.py

--- Activity18.py
+++ Activity18.py
@@ -1,10 +1,11 @@
-#repeated triangle
-
-nt= eval(input("Enter number of triangle-->"))
-for x in range (1,5):
-   for r in range (1,nt+1):
-       for y in range (1,x+1):
-            print("*",end =" ")
-       for z in range (5,x,-1):
-            print(" "),end=" ")
-   print()
+     #repeated triangle
+def Activity18():
+     nt= eval(input("Enter number of triangle-->"))
+     for x in range (1,5):
+     for r in range (1,nt+1):
+          for y in range (1,x+1):
+               print("*",end =" ")
+          for z in range (5,x,-1):
+               print(" "),end=" ")
+     print()
+Activity18()
