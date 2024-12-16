f116a8852fe4d076c8ab1df966cc641e37fc7bd0
Savvsavv <demesasavannah@gmail.com>
2024-12-17 07:51:45 +0800

    Update Code_Challenge2.py

--- Code_Challenge2.py
+++ Code_Challenge2.py
@@ -1,3 +1,5 @@
-name=input("Please input your name here -->")
+def Code_Challenge2():
+    name=input("Please input your name here -->")
 
-print("\t\t\t\t\t\t\t\t\t\t\t* \n\t\t\t\t\t\t\t\t\t\t\t\b    *   *   * \n\t\t\t\t\t\t\t\t\t\t\b        *   *   *   *   * \n\t\t\t\t\t\t\t\t\t\t\b    *   *  Hi "+ name," *   * \n\t\t\t\t\t\t\t\t\t\t\b        *   *   *   *   * \n\t\t\t\t\t\t\t\t\t\t\t\b    *   *   * \n\t\t\t\t\t\t\t\t\t\t\t*")
+    print("\t\t\t\t\t\t\t\t\t\t\t* \n\t\t\t\t\t\t\t\t\t\t\t\b    *   *   * \n\t\t\t\t\t\t\t\t\t\t\b        *   *   *   *   * \n\t\t\t\t\t\t\t\t\t\t\b    *   *  Hi "+ name," *   * \n\t\t\t\t\t\t\t\t\t\t\b        *   *   *   *   * \n\t\t\t\t\t\t\t\t\t\t\t\b    *   *   * \n\t\t\t\t\t\t\t\t\t\t\t*")
+Code_Challenge2()
