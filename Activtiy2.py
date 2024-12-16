f803ee2c2f30772a28706d00b17a8a06db9a5e65
Savvsavv <demesasavannah@gmail.com>
2024-12-17 07:41:05 +0800

    Update Activtiy2.py

--- Activtiy2.py
+++ Activtiy2.py
@@ -1,4 +1,6 @@
-num1= eval (input("Please input a number here --> "))
-num2= eval(input("Please input another number --> "))
+def Activity2():
+    num1= eval (input("Please input a number here --> "))
+    num2= eval(input("Please input another number --> "))
 
-print(num1, "Floor division to " num1, " = ", num1 // num2)
+    print(num1, "Floor division to ", num1, " = ", num1 // num2)
+Activity2()
