c97c648b24d530d81ef890cfe7549d246cfdc158
Savvsavv <demesasavannah@gmail.com>
2024-12-17 07:52:55 +0800

    Update code_challenge4.py

--- code_challenge4.py
+++ code_challenge4.py
@@ -1,19 +1,21 @@
-print("\n\t|~~~~~~~~~~~~~|\n\t|By: Savannah | \n\t|~~~~~~~~~~~~~|")
+def Code_Challenge4():
+    print("\n\t|~~~~~~~~~~~~~|\n\t|By: Savannah | \n\t|~~~~~~~~~~~~~|")
 
-n1 =eval(input ("\n  enter the First Number --> "))
-n2 =eval(input("  enter the Second Number --> "))
-ans1 = n1 + n2
-ans2 = n1 - n2
-ans3 = n1 * n2
-ans4 = n1 / n2
-ans5 = n1 ** n2
-ans6 = n1 // n2
-ans7 = n1 % n2
+    n1 =eval(input ("\n  enter the First Number --> "))
+    n2 =eval(input("  enter the Second Number --> "))
+    ans1 = n1 + n2
+    ans2 = n1 - n2
+    ans3 = n1 * n2
+    ans4 = n1 / n2
+    ans5 = n1 ** n2
+    ans6 = n1 // n2
+    ans7 = n1 % n2
 
-print ("\n\t The sum of" ,n1 ,"and" ,n2 , "is " ,ans1 ,"\b.")
-print ("\n\t The difference of" ,n1 , "and" ,n2 , "is " ,ans2 , "\b.")
-print ("\n\t The product of" ,n1 , "and" ,n2 , "is " ,ans3 , "\b.")
-print ("\n\t The quotient of" ,n1 , "and" ,n2 , "is ",ans4 , "\b.")
-print("\n\t",n1 , "exponent by" ,n2 , "is " ,ans5 , "\b.")
-print ("\n\t The remainder of" ,n1 , "and" ,n2 , "is ",ans7 , "\b.")
-print ("\n\t The floor division of" ,n1 , "and" ,n2 , "is ",ans6 , "\b.")
+    print ("\n\t The sum of" ,n1 ,"and" ,n2 , "is " ,ans1 ,"\b.")
+    print ("\n\t The difference of" ,n1 , "and" ,n2 , "is " ,ans2 , "\b.")
+    print ("\n\t The product of" ,n1 , "and" ,n2 , "is " ,ans3 , "\b.")
+    print ("\n\t The quotient of" ,n1 , "and" ,n2 , "is ",ans4 , "\b.")
+    print("\n\t",n1 , "exponent by" ,n2 , "is " ,ans5 , "\b.")
+    print ("\n\t The remainder of" ,n1 , "and" ,n2 , "is ",ans7 , "\b.")
+    print ("\n\t The floor division of" ,n1 , "and" ,n2 , "is ",ans6 , "\b.")
+Code_Challenge4()
