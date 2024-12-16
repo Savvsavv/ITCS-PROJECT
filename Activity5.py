2cc940a53e4e555767d97b4015b779c089fd787e
Savvsavv <demesasavannah@gmail.com>
2024-12-16 21:44:02 +0800

    Update Activity5.py

--- Activity5.py
+++ Activity5.py
@@ -1,6 +1,8 @@
-temp=int(input("Enter temperature in Farenheit: "))
+def Activity5():
+    temp=int(input("Enter temperature in Farenheit: "))
 
-cel = (temp-32)*5/9
-round =(round(cel,2))
+    cel = (temp-32)*5/9
+    round =(round(cel,2))
 
-print(f"The conversion of {temp} degrees farenheit is {round} degrees celcius")
+    print(f"The conversion of {temp} degrees farenheit is {round} degrees celcius")
+Activity5()
