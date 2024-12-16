a28214f6903d96e28cda7ea3c811bd80650810f3
Savvsavv <demesasavannah@gmail.com>
2024-12-16 22:07:03 +0800

    Update Activity15.py

--- Activity15.py
+++ Activity15.py
@@ -1,9 +1,11 @@
-for x in range (0,11):
-  print(x,"\t", end=" ")
-  for y in range (0,x):
-    print ("*",end=" ")
-  print ()
-for x in range (10,0,-1):
-  for y in range (0,x):
-    print(" * ",end=" ")
-  print()
+def Activity15():
+  for x in range (0,11):
+    print(x,"\t", end=" ")
+    for y in range (0,x):
+      print ("*",end=" ")
+    print ()
+  for x in range (10,0,-1):
+    for y in range (0,x):
+      print(" * ",end=" ")
+    print()
+Activity15()
