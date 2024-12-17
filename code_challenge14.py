0e735556fb1697287956ca2a98bea01d71d03fe9
Savvsavv <demesasavannah@gmail.com>
2024-12-17 08:23:31 +0800

    Update code_challenge14.py

--- code_challenge14.py
+++ code_challenge14.py
@@ -1,17 +1,19 @@
-# ask user for  number
-total_sum = 0
+def Code_Challenge14():
+    # ask user for  number
+    total_sum = 0
 
-# Loop to get numbers from user
-while True:
-    number = int(input("Enter a number --> "))
-    
-    # If the entered number is 0, terminate the loop
-    if number == 0:
-        print("Loop terminated")
-        break
-    
-    # Add the number to the total sum
-    total_sum += number
+    # Loop to get numbers from user
+    while True:
+        number = int(input("Enter a number --> "))
+        
+        # If the entered number is 0, terminate the loop
+        if number == 0:
+            print("Loop terminated")
+            break
+        
+        # Add the number to the total sum
+        total_sum += number
 
-# Print total sum
-print(f"The sum of all the numbers given is {total_sum}")
+    # Print total sum
+    print(f"The sum of all the numbers given is {total_sum}")
+Code_Challenge14()
