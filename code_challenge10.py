18e32cb8b8aac2861d11c4bad8a6b5de46b1d7e1
Savvsavv <demesasavannah@gmail.com>
2024-12-17 08:15:51 +0800

    Update code_challenge10.py

--- code_challenge10.py
+++ code_challenge10.py
@@ -1,19 +1,21 @@
-#diamond na hindi pointed
-#arrow up
-for x in range (1,6):
-    for y in range (6, x,-1):
-        print (" " , end= " ")
-    for z in range (1,x+1):
-        print ("*", end= " ")
-    for a in range ( 1, x+1):
-        print ("*", end= " ")
-    print ()
-#arrow down
-for x in range (1,6):
-    for y in range (1, x+1):
-        print (" " , end= " ")
-    for z in range (6,x,-1):
-        print ("*", end= " ")
-    for a in range (6, x,-1):
-        print ("*" , end= " ")
-    print ()
+def Code_Challenge10():
+    #diamond na hindi pointed
+    #arrow up
+    for x in range (1,6):
+        for y in range (6, x,-1):
+            print (" " , end= " ")
+        for z in range (1,x+1):
+            print ("*", end= " ")
+        for a in range ( 1, x+1):
+            print ("*", end= " ")
+        print ()
+    #arrow down
+    for x in range (1,6):
+        for y in range (1, x+1):
+            print (" " , end= " ")
+        for z in range (6,x,-1):
+            print ("*", end= " ")
+        for a in range (6, x,-1):
+            print ("*" , end= " ")
+        print ()
+Code_Challenge10()
