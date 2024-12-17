1689aad64a6a683107de8986e9073a9d37b83c53
Savvsavv <demesasavannah@gmail.com>
2024-12-17 08:10:37 +0800

    Update code_challenge6.py

--- code_challenge6.py
+++ code_challenge6.py
@@ -1,16 +1,19 @@
-#grades
+def Code_Challenge6():
+	#grades
+	prelim = int(input("Enter your Prelim Grade: "))
+	midterm = int(input("Enter your Midterm Grade: "))
+	sfinals = int(input("Enter your Semi Finals Grade: "))
+	finals = int(input("Enter your Finals Grade: "))
+	quiz = int(input("Enter your Quiz Grade: "))
+	project = int(input("Enter your Project Grade: "))
 
-prelim =eval(input(" Enter your Prelim Grade : "))
-midterm = eval(input("Enter your Midterm Grade : "))
-sfinals =eval(input("Enter your Semi Finals Grade : "))
-finals =eval(input(" Enter your Finals Grade : "))
-quiz =eval(input(" Enter your Quiz Grade : "))
-project =eval(input(" Enter your Project Grade : "))
+	if (0 <= prelim <= 100) and (0 <= midterm <= 100) and (0 <= sfinals <= 100) and (0 <= finals <= 100) and (0 <= quiz <= 100) and (0 <= project <= 100):
+		fg = (prelim * 0.15) + (midterm * 0.15) + (sfinals * 0.15) + (finals * 0.15) + (quiz * 0.25) + (project * 0.15)
 
-
-if (prelim >= 65 and prelim <=100) and (midterm >= 65 and midterm <=100) and (sfinals >= 65 and sfinals <=100) and (finals >= 65 and finals <=100) and (quiz >= 65 and quiz <=100) and (project >= 65 and project <=100):
-	fg =(prelim *0.15) + (midterm *0.15)  + (sfinals *0.15) + (finals *0.15) + (quiz *0.25) + (project *0.15) 
-	if fg >=75:
-		print("Congratulations, you passed!")
+		if fg >= 75:
+				print("Congratulations, you passed!")
+		else:
+				print("Sorry, you failed.")
 	else:
-		print("Sorry, you failed")
+		print("Error: All grades must be between 0 and 100.")
+Code_Challenge6()
