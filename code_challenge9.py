796ba7e314a7b9c28ef81dea8d04198220d6c55c
Savvsavv <demesasavannah@gmail.com>
2024-12-17 08:14:49 +0800

    Update code_challenge9.py

--- code_challenge9.py
+++ code_challenge9.py
@@ -1,9 +1,10 @@
-#inverted half pyramid 
+    #inverted half pyramid 
 
-
-for x in range (10):
-    for y in range (2*x):
-        print (" " , end= "")
-    for space in range (10-x):
-        print ("* ", end= "")
-    print ()
+def Code_Challenge9():
+    for x in range (10):
+        for y in range (2*x):
+            print (" " , end= "")
+        for space in range (10-x):
+            print ("* ", end= "")
+        print ()
+Code_Challenge9()
