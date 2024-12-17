a679b3c1e8d08964300cc8c35fbf9505129b6ef9
Savvsavv <demesasavannah@gmail.com>
2024-12-17 15:47:31 +0800

    act1 change

--- Activity1.py
+++ Activity1.py
@@ -1,3 +1,3 @@
 def Activity1():
     print("\t\t\t\t\t\t\t\t\t\t\t* \n\t\t\t\t\t\t\t\t\t\t\t\b    *   *   * \n\t\t\t\t\t\t\t\t\t\t\b        *   *   *   *   *  \n\t\t\t\t\t\t\t\t\t\t\b    *   *   *   *   *   *   * \n\t\t\t\t\t\t\t\t\t\t\b      *   *   *   *   * \n\t\t\t\t\t\t\t\t\t\t\t\b    *   *   * \n\t\t\t\t\t\t\t\t\t\t\t*")
-Activity1()
+
