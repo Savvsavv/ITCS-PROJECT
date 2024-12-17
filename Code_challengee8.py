cd2727268df56f904e992a9785f21c8df12d17d3
Savvsavv <demesasavannah@gmail.com>
2024-12-17 08:25:46 +0800

    Update Code_challengee8.py

--- Code_challengee8.py
+++ Code_challengee8.py
@@ -1,8 +1,10 @@
-# random numbers and getting the summation of it
+def Code_Challenge8():
+	# random numbers and getting the summation of it
 
-sum =0
-for x in range (1,11):
-	num = eval(input(f"Enter #{x} : "))
-	sum +=num
+	sum =0
+	for x in range (1,11):
+		num = eval(input(f"Enter #{x} : "))
+		sum +=num
 
-print(f"The sum of all the numbers given is {sum} ")
+	print(f"The sum of all the numbers given is {sum} ")
+Code_Challenge8()
