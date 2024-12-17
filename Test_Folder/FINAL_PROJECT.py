d777a82d76d5851d549ada14e9b7fc6368f880d8
Savvsavv <demesasavannah@gmail.com>
2024-12-17 17:54:19 +0800

    CHANGE

--- Test_Folder/FINAL_PROJECT.py
+++ Test_Folder/FINAL_PROJECT.py
@@ -9,7 +9,26 @@ from Code_Challenges_Folder import (code_challenge1, Code_Challenge2, code_chall
                                     code_challenge6, code_challenge7, Code_challengee8, code_challenge9, 
                                     code_challenge10, code_challenge11, code_challenge12, code_challenge13,
                                     code_challenge14,code_challenge15,code_challenge16)
+#Main Menu
 def Main_Menu():
     while True:
-        try:
-            print("")
+            print(" ~~~~~~~~~~~~ PROJECT COMPILATION ~~~~~~~~~~~~~"
+                  "~~~~~~~~~~~~~~~~~~~~~MENU~~~~~~~~~~~~~~~~~~~~~~"
+                  
+                  "<1> - ACTIVITY_FOLDER"
+                  "<2> - CODE_CHALLENGES_FOLDER"
+                  "<0> - TERMINATE")
+            
+            try:
+                number = int(input("Choose what you want to open:"))
+                if number ==1:
+                        pass
+                elif number ==2:
+                        pass
+                elif number ==0:
+                    print("\n\tPROGRAM TERMINATED. THANK YOU FOR USING THIS PROGRAM FOR A SHORT TIME")
+                    break
+                else:
+                    print("\n\tERROR!!! PLEASE CHOOSSE ONE OPTION ABOVE THAT IS NOT IN THE OPTION.")
+            except ValueError:
+                  print("\n\t ERROR. ERROR. ERROR. INVALID INPUT")
